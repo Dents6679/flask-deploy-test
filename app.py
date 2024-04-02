73f20e18468f16ca2a4fd67ee0fb028678d9cf0e
Tom Dent <tomdent6679@gmail.com>
2024-04-02 16:57:27 +0100

    Added redirect for static file.

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return send_from_directory('static', 'index.html')
+def hello_world():
+
+    return send_from_directory('templates', 'index.html')
 
 
 if __name__ == '__main__':
